aa9bb3192aa6d591298a402422d762a7a8dc5cbe
Philipp Lang <plang@slb.com>
2018-08-16 10:43:52 +0100

    clean code snippet

--- README.md
+++ README.md
@@ -11,12 +11,23 @@ The interface is intentionally kept to a minmum: A `Run` instance is tied to one
 
 ```
 tbproto::Run run;
-tbproto::Record rec0, rec1;
-rec0.add("my/value", tbproto::Scalar(0.0));
-run.write(rec0);
-rec1.set_step(1);
-rec1.add("my/value", tbproto::Scalar(1.0));
-run.write(rec1);
+
+// first iteration (e.g.)
+{
+tbproto::Record rec;
+rec.set_step(0);
+rec.add("my/value", tbproto::Scalar(0.0));
+rec.add("my/othervalue", tbproto::Scalar(1.0));
+run.write(rec);
+}
+// second iteration (e.g.)
+{
+tbproto::Record rec;
+rec.set_step(1);
+rec.add("my/value", tbproto::Scalar(1.0));
+rec.add("my/othervalue", tbproto::Scalar(0.8));
+run.write(rec);
+}
 ```
 
 ## Installation
--- main.cpp
+++ main.cpp
@@ -5,12 +5,23 @@
 
 int main(int argc, char const *argv[]) {
   tbproto::Run run;
-  tbproto::Record rec0, rec1;
-  rec0.add("my/value", tbproto::Scalar(0.0));
-  run.write(rec0);
-  rec1.set_step(1);
-  rec1.add("my/value", tbproto::Scalar(1.0));
-  run.write(rec1);
+
+  // first iteration (e.g.)
+  {
+    tbproto::Record rec;
+    rec.set_step(0);
+    rec.add("my/value", tbproto::Scalar(0.0));
+    rec.add("my/othervalue", tbproto::Scalar(1.0));
+    run.write(rec);
+  }
+  // second iteration (e.g.)
+  {
+    tbproto::Record rec;
+    rec.set_step(1);
+    rec.add("my/value", tbproto::Scalar(1.0));
+    rec.add("my/othervalue", tbproto::Scalar(0.8));
+    run.write(rec);
+  }
 
   return 0;
 }
